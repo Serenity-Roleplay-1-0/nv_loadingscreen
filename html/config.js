d8231ccc5a9ec22b4b10daadf2b052da4370b97e
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2024-12-23 01:34:19 -0700

    Add files via upload

---
+++ html/config.js
@@ -0,0 +1,98 @@
+/*=================================================================*\
+/* By: 			|	Nevera Development  							|
+/* FiveM: 		|	https://forum.cfx.re/u/neveradevelopment		|
+/* Discord: 	|	https://discord.gg/NeveraDev/tw28AqrgWU  		|
+/*=================================================================*/
+/* If you have any problems you can contact us via discord. <3     */
+
+var staff_team = [
+	{
+		"name":"AdamaStark_N7",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/Serenity_Loading_Screen.png",
+		"rank":"Server Owner"
+	},
+	{
+		"name":"AdamaStark_N7",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/images/Loading_Screen.png",
+		"rank":"Lead Server Developer"
+	},
+	{
+		"name":"Cattt",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/8-zmcM4d6g0yctBfw.png",
+		"rank":"Lead Server Admin"
+	},
+	{
+		"name":"Sgt_Nathanial",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/Serenity_Loading_Screen.png",
+		"rank":"Lead Server Moderator"
+	},
+	{
+		"name":"Monkey595",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/Serenity_Loading_Screen.png",
+		"rank":"Lead Server Tester"
+	}
+]
+
+// Staff Settings
+const showStaffTeam = true
+const showPlayersList = true
+
+// orange
+// red
+// blue
+// green
+// pink
+// purple
+const theme = "green"
+
+// ==== WINTER UPDATE !! ==== \\
+const enableWinterUpdate = true
+// ==== WINTER UPDATE !! ==== \\
+
+// Text settings
+const name = "<strong>SERE</strong>NITY" // Serenity
+const underName = "ROLE<b>PLAY</b>"
+const desc = "A unique FiveM Roleplay server currently in beta development featuring an extensive collection of custom scripts, maps, vehicles, unique weapons, and much more!"
+
+// Social media
+const discord = "https://discord.gg/Cmakb5udQg" // If = "" then icon will not show up on screen
+const instagram = ""	// https://example.com
+const youtube = "" 		// https://example.com
+const twitter = "" 		// https://example.com
+const tiktok = ""  		// https://example.com
+const facebook = ""		// https://example.com
+const twitch = "" 		// https://example.com
+const github = "https://github.com/Serenity-Roleplay-1-0" // https://example.com
+
+// Player List
+const serverCode = "av7v49" //Your server CODE from fivem. (cfx.re/join/abcdef) this ABCDEF is your code, paste it in serverCode.
+const playerProfileImage = "https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/Serenity_Loading_Screen.png"
+
+// Video Settings
+const videoBlur = 0
+var videoOpacity = 0.3
+
+// Example link: https://www.youtube.com/watch?v=abcdefgh
+
+const youtubeVideo = "https://www.youtube.com/watch?v=SztA0gUsvOw" // Running Wild
+// const youtubeVideo = "https://www.youtube.com/watch?v=Q_5z4n5TZ2Q" // Sleepwalking
+// const youtubeVideo = "https://www.youtube.com/watch?v=KrM107U_uRg" // Street Lights
+const showYoutubeVideo = false
+
+// Local Video
+const enableLocalVideo = false
+
+// Local audio
+const localAudio = false
+
+// HELP //
+
+//-- YOUTBE
+//-- LOCAL AUDIO
+// if localAudio is true, then loading will load "audio.mp3" file and play it except youtube audio.
+// if localAudio is false, then loading will load youtube audio.
+
+//-- LOCAL VIDEO
+// if enableLocalVideo is true, then loading will load "video.webm" file and play it except youtube video.
+// If localVideo is enabled, showYoutubeVideo is automatically disabled.
+// You can only import a video from either YouTube or local. Local video taking priority.
